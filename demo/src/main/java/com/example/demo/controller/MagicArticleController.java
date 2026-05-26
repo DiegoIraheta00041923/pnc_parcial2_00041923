ddfd56c4ac655e5fd804f95a84e94ae343cd6689
DiegoIraheta00041923 <128827201+DiegoIraheta00041923@users.noreply.github.com>
2026-05-25 20:09:11 -0600

    feat: add delete and create Article

--- demo/src/main/java/com/example/demo/controller/MagicArticleController.java
+++ demo/src/main/java/com/example/demo/controller/MagicArticleController.java
@@ -1,9 +1,53 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.domain.request.CreateMagicArticleRequest;
+import com.example.demo.domain.response.GeneralResponse;
+import com.example.demo.service.MagicArticleServiceImp;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/artefacts")
+@AllArgsConstructor
 public class MagicArticleController {
+
+    private MagicArticleServiceImp magicArticleImp;
+
+    @PostMapping
+    public ResponseEntity<GeneralResponse> createMagicArticle(@RequestBody CreateMagicArticleRequest request){
+        return buildResponse(
+                "Magic Article created!",
+                HttpStatus.OK,
+                magicArticleImp.createMagicArticle(request)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GeneralResponse> deleteArticle(@PathVariable UUID id){
+        return buildResponse("Article deleted!",
+                HttpStatus.OK,
+                magicArticleImp.deleteMagicArticle(id));
+    }
+
+
+    
+    public ResponseEntity<GeneralResponse> buildResponse(String message, HttpStatus status, Object data) {
+        String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().getPath();
+        return ResponseEntity
+                .status(status)
+                .body(GeneralResponse.builder()
+                        .uri(uri)
+                        .message(message)
+                        .status(status.value())
+                        .time(LocalDateTime.now())
+                        .data(data)
+                        .build()
+                );
+    }
 }
